a7ef5daaf4a99b84d1abeb83ca74447a8c030018
Aaron Brayall <abrayall@yahoo.com>
2017-07-06 21:56:51 -0400

    updating with support for downloading and unzip JREs

---
+++ src/main/java/jetpack/jre/Jre.java
@@ -0,0 +1,74 @@
+package jetpack.jre;
+
+import static javax.util.List.*;
+
+import java.net.URL;
+
+import javax.io.File;
+import javax.util.List;
+
+public class Jre {
+	
+	public static String VERSION_1_8 = "1.8.0_131";
+	public static List<String> VERSIONS = list(VERSION_1_8);
+	
+	public static String PLATFORM_LINUX = "linux";
+	public static String PLATFORM_WINDOWS = "windows";
+	public static String PLATFORM_MACOS = "osx";
+	public static List<String> PLATFORMS = list(PLATFORM_LINUX, PLATFORM_WINDOWS, PLATFORM_MACOS);
+	
+	public static String BIT_32 = "32";
+	public static String BIT_64 = "64";
+	public static List<String> BITS = list(BIT_32, BIT_64);
+	
+	protected String version;
+	protected String platform;
+	protected String bits;
+	
+	protected URL url;
+	protected File path;
+
+	protected Jre(String version, String platform, String bits, URL url, File path) {
+		this.version = version;
+		this.platform = platform;
+		this.bits = bits;
+		this.url = url;
+		this.path = path;
+	}	
+	
+	public boolean equals(Object object) {
+		return this.toString().equals(object.toString());
+	}
+	
+	public boolean equals(String version, String platform, String bits) {
+		return this.version.equals(version) && this.platform.equals(platform) && this.bits.equals(bits);
+	}
+	
+	public String toString() {
+		return "jre [v" + this.version + " " + this.platform + " " + this.bits + "bits]";
+	}
+
+	public String getVersion() {
+		return this.version;
+	}
+
+	public String getPlatform() {
+		return this.platform;
+	}
+
+	public String getBits() {
+		return this.bits;
+	}
+
+	public URL getUrl() {
+		return this.url;
+	}
+
+	public File getPath() {
+		return this.path;
+	}
+	
+	public File getJava() {
+		return new File(path, "bin/java" + (this.platform.equals(Jre.PLATFORM_WINDOWS) ? ".exe" : ""));
+	}
+}
---
+++ src/main/java/jetpack/jre/JreManager.java
@@ -0,0 +1,114 @@
+package jetpack.jre;
+
+import static javax.io.File.*;
+import static javax.util.Map.*;
+import static javax.util.zip.ZipFile.*;
+
+import java.io.PrintStream;
+import java.net.URL;
+import java.util.function.BiConsumer;
+import java.util.zip.ZipEntry;
+
+import javax.io.File;
+import javax.lang.Strings;
+import javax.net.Urls;
+import javax.util.List;
+
+public class JreManager {
+	
+	private File work;
+	
+	public JreManager(File work) {
+		this.work = work;
+	}
+	
+	public List<Jre> list() throws Exception {
+		List<Jre> jres = List.list();
+		for (String version : Jre.VERSIONS) {
+			for (String platform : Jre.PLATFORMS) {
+				for (String bits : Jre.BITS) {
+					if (platform.equals(Jre.PLATFORM_MACOS) && bits.equals(Jre.BIT_64))
+						continue;
+					
+					jres.add(new Jre(version, platform, bits,
+						url(version, platform, bits),
+						path(version, platform, bits)
+					));
+				}
+			}
+		}
+	
+		return jres;
+	}
+
+	public Jre get(String version, String platform) throws Exception {
+		return this.get(platform, version, Jre.BIT_64);
+	}
+	
+	public Jre get(String version, String platform, String bits) throws Exception {
+		for (Jre jre : this.list()) {
+			if (jre.equals(version, platform, bits)) 
+				return jre;
+		}
+		
+		return null;
+	}
+	
+	public Jre download(Jre jre) throws Exception {
+		return this.download(jre, (bytes, total) -> {}, (entry, file) -> {});
+	}
+	
+	public Jre download(Jre jre, BiConsumer<Long, Long> download, BiConsumer<ZipEntry, File> extract) throws Exception {
+		unzip(Urls.download(jre.url, file(jre.path, "jre.zip"), download), file(jre.path, "jre"), (entry, file) -> {
+			File resolved = resolve(jre, entry, file);
+			extract.accept(entry, resolved);
+			return resolved;
+		});
+		
+		return jre;
+	}
+
+	protected String name(Jre jre) {
+		return name(jre.version, jre.platform, jre.bits);
+	}
+	
+	protected String name(String version, String platform, String bits) {
+		return "jre" + version + "-" + platform + (bits.equals(Jre.BIT_64) ? "-x64" : "");
+	}
+	
+	protected URL url(String version, String platform, String bits) {
+		return Urls.url("https://installbuilder.bitrock.com/java/" + name(version, platform, bits) + ".zip");
+	}
+	
+	protected File path(String version, String platform, String bits) {
+		return file(work, "/java/" + platform + "/" + version + "/" + bits + "bit");
+	}
+	
+	protected File resolve(Jre jre, ZipEntry entry, File file) {
+		return file(file.path().replace(name(jre) + "/", "").replace("java-" + jre.platform + (jre.bits.equals(Jre.BIT_64) ? "-x64" : ""), ""));
+	}
+	
+	public static void main(String[] arguments) throws Exception {
+		JreManager manager = new JreManager(new File("/tmp/java"));
+		Jre jre = manager.get(Jre.VERSION_1_8, Jre.PLATFORM_LINUX, Jre.BIT_64);
+		//if (jre.path.exists() == false)
+			manager.download(jre, JreManager::downloaded, JreManager::extracted);
+			
+	}
+	
+	public static BiConsumer<Long, Long> downloaded(String format) {
+		return downloaded(System.out, format);
+	}
+
+	public static BiConsumer<Long, Long> downloaded(PrintStream stream, String format) {
+		return (bytes, size) -> stream.println(Strings.format(format, map(entry("bytes", bytes), entry("size", size))));		
+	}
+			
+	public static void downloaded(Long bytes, Long size) {
+		System.out.println("Downloaded " + bytes + " bytes out of " + size + " bytes [" + String.format("%.2f", ((bytes.doubleValue() / size.doubleValue()) * 100.0)) + "%]");
+	}
+	
+	public static void extracted(ZipEntry entry, File file) {
+		System.out.println("Extracting " + entry + " to " + file);
+	}
+}
