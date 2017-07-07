781675417a11be6dc13dfdca0f1e53556d761f24
Aaron Brayall <abrayall@yahoo.com>
2017-07-06 21:58:02 -0400

    updating launch4j support with ability to actually run launch4j

--- src/main/java/jetpack/launch4j/Launch4J.java
+++ src/main/java/jetpack/launch4j/Launch4J.java
@@ -1,9 +1,15 @@
 package jetpack.launch4j;
 
-import java.io.File;
+import static javax.io.File.*;
+import static javax.util.List.*;
 
+import javax.io.File;
+import javax.lang.System;
+
+import jetpack.Configuration;
 import net.sf.launch4j.Builder;
 import net.sf.launch4j.Log;
+import net.sf.launch4j.config.ClassPath;
 import net.sf.launch4j.config.Config;
 import net.sf.launch4j.config.ConfigPersister;
 import net.sf.launch4j.config.Jre;
@@ -11,17 +17,61 @@ import net.sf.launch4j.config.Jre;
 public class Launch4J {
 
 	public static void main(String[] arguments) throws Exception  {
+		Launch4J launch4j = new Launch4J();
+		System.out.println(launch4j.build(new Configuration()
+			.setName("jetpack")
+			.setMain("jetpack.Main")
+			.setJar("build/artifacts/jetpack.jar"), 
+		file("build")));
+	}
+	
+	public File build(Configuration configuration, File directory) throws Exception {
+		Logger logger = new Logger();
+		Config config = config(configuration, directory);
+		File output = file(config.getOutfile()).mkdirs();
+
+		Builder builder = builder(config, logger);
+		builder.build();
+		
+		return output.exists() && logger.results().contains("Successfully created") ?  output : null;
+	}
+	
+	public Builder builder(Config config, Logger logger) {
+		return new Builder(logger, new File("resources/launch4j/" + (System.isWindows() ? "windows" : "linux")).toFile().getAbsoluteFile());
+	}
+	
+	public Config config(Configuration configuration, File directory) {
 		ConfigPersister.getInstance().createBlank();
 		Config config = ConfigPersister.getInstance().getConfig();
 
 		Jre jre = new Jre();
-		jre.setMinVersion("1.8.0");
+		jre.setPath("1.8.0");
 		
-		config.setJar(new File("build/artifacts/jetpack.jar"));
-		config.setOutfile(new File("test.exe"));
+		ClassPath classpath = new ClassPath();
+		classpath.setMainClass(configuration.getMain());
+		classpath.setPaths(list());
+		
+		config.setClassPath(classpath);
+		config.setJar(file(configuration.getJar()).toFile());
+		config.setOutfile(file(directory, "windows/" + configuration.getName() + ".exe").toFile());
 		config.setJre(jre);
+		return config;
+	}
+	
+	public static class Logger extends Log {
 		
-		Builder builder = new Builder(Log.getAntLog(), new File("resources/launch4j/linux"));
-		builder.build();
+		protected StringBuilder output = new StringBuilder();
+
+		public void append(String message) {
+			this.output.append(message + "\n");
+		}
+
+		public void clear() {
+			this.append("\n");
+		}	
+		
+		public String results() {
+			return this.output.toString();
+		}
 	}
 }
