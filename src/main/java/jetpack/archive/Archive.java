01be88e3d3126c42e2465df47e96511f1d79b884
Aaron Brayall <abrayall@yahoo.com>
2017-07-08 07:59:06 -0400

    updating with tar and zip file generation support

---
+++ src/main/java/jetpack/archive/Archive.java
@@ -0,0 +1,64 @@
+package jetpack.archive;
+
+import static javax.io.File.*;
+import static javax.util.List.*;
+
+import java.io.OutputStream;
+
+import javax.io.File;
+import javax.io.Streams;
+
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+
+public class Archive {
+
+	public static File tar(File directory) throws Exception {
+		return tar(directory, file(directory.parent(), directory.name() + ".tar"));
+	}
+	
+	public static File tar(File directory, File file) throws Exception {
+		return tar(directory, file, ".*");
+	}
+	
+	public static File tar(File directory, File file, String pattern) throws Exception {
+		archive(directory, new TarArchiveOutputStream(file.outputStream()), pattern);
+		return file;
+	}
+	
+	public static File zip(File directory) throws Exception {
+		return zip(directory, file(directory.parent(), directory.name() + ".zip"));
+	}
+	
+	public static File zip(File directory, File file) throws Exception {
+		return zip(directory, file, ".*");
+	}
+	
+	public static File zip(File directory, File file, String pattern) throws Exception {
+		archive(directory, zip(file.outputStream()), pattern);
+		return file;
+	}
+	
+	protected static ZipArchiveOutputStream zip(OutputStream outputStream) {
+		ZipArchiveOutputStream zip = new ZipArchiveOutputStream(outputStream);
+		zip.setLevel(1);
+		return zip;
+	}
+	
+	protected static void archive(File directory, ArchiveOutputStream archive, String pattern) throws Exception {
+		for (File child : list(directory.search(pattern))) {
+			archive.putArchiveEntry(archive.createArchiveEntry(child.toFile(), child.relative(directory)));
+			if (child.isDirectory() == false)
+				Streams.copy(child.inputStream(), archive);
+			
+			archive.closeArchiveEntry();
+		}
+		
+		archive.close();
+	}
+	
+	public static void main(String[] arguments) throws Exception {
+		zip(file("/tmp/treehouse"));
+	}
+}
