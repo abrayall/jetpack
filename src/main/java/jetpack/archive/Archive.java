7c2dc6bfd0903a882e67747f602c6019a9a8d78e
Aaron Brayall <abrayall@yahoo.com>
2017-07-08 09:26:34 -0400

    updating archive integration to support layouts were the files are not all staged in the same place

--- src/main/java/jetpack/archive/Archive.java
+++ src/main/java/jetpack/archive/Archive.java
@@ -2,11 +2,13 @@ package jetpack.archive;
 
 import static javax.io.File.*;
 import static javax.util.List.*;
+import static javax.util.Map.*;
 
 import java.io.OutputStream;
 
 import javax.io.File;
 import javax.io.Streams;
+import javax.util.Map;
 
 import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
@@ -23,7 +25,16 @@ public class Archive {
 	}
 	
 	public static File tar(File directory, File file, String pattern) throws Exception {
-		archive(directory, new TarArchiveOutputStream(file.outputStream()), pattern);
+		archive(directory, new TarArchiveOutputStream(file.outputStream()), "", pattern).close();
+		return file;
+	}
+	
+	public static File tar(File file, Map<String, File> layout) throws Exception {
+		return tar(file, layout, ".*");
+	}
+	
+	public static File tar(File file, Map<String, File> layout, String pattern) throws Exception {
+		archive(new TarArchiveOutputStream(file.outputStream()), layout, pattern).close();
 		return file;
 	}
 	
@@ -36,7 +47,16 @@ public class Archive {
 	}
 	
 	public static File zip(File directory, File file, String pattern) throws Exception {
-		archive(directory, zip(file.outputStream()), pattern);
+		archive(directory, zip(file.outputStream()), "", pattern).close();
+		return file;
+	}
+	
+	public static File zip(File file, Map<String, File> layout) throws Exception {
+		return zip(file, layout, ".*");
+	}
+	
+	public static File zip(File file, Map<String, File> layout, String pattern) throws Exception {
+		archive(zip(file.outputStream()), layout, pattern).close();
 		return file;
 	}
 	
@@ -46,19 +66,38 @@ public class Archive {
 		return zip;
 	}
 	
-	protected static void archive(File directory, ArchiveOutputStream archive, String pattern) throws Exception {
+	protected static ArchiveOutputStream archive(ArchiveOutputStream archive, Map<String, File> layout, String pattern) throws Exception {
+		for (String entry : layout.keySet())
+			archive(layout.get(entry), archive, entry.endsWith("/") ? entry : entry + "/", pattern);
+		
+		return archive;
+	}
+	
+	protected static ArchiveOutputStream archive(File directory, ArchiveOutputStream archive, String prefix, String pattern) throws Exception {
 		for (File child : list(directory.search(pattern))) {
-			archive.putArchiveEntry(archive.createArchiveEntry(child.toFile(), child.relative(directory)));
+			archive.putArchiveEntry(archive.createArchiveEntry(child.toFile(), prefix + child.relative(directory)));
 			if (child.isDirectory() == false)
 				Streams.copy(child.inputStream(), archive);
 			
 			archive.closeArchiveEntry();
 		}
 		
-		archive.close();
+		return archive;
 	}
 	
 	public static void main(String[] arguments) throws Exception {
 		zip(file("/tmp/treehouse"));
+		tar(file("/tmp/treehouse"));
+
+		tar(file("/tmp/test.tar"), map(
+			entry("lib", file("/tmp/treehouse")),
+			entry("simpliprotected", file("/tmp/simpliprotected"))
+		));
+		
+		zip(file("/tmp/test.zip"), map(
+			entry("lib", file("/tmp/treehouse")),
+			entry("simpliprotected", file("/tmp/simpliprotected"))
+		));
+		
 	}
 }
